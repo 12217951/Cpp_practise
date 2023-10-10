6fe4e4a114c2b249c7da7386398c96b26f325b06
Smita <114800220+12217951@users.noreply.github.com>
2023-10-10 22:46:54 +0530

    Add files via upload

---
+++ arrayOfStruscture.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+struct emp //Structure name(emp)
+{
+    string name;//structure member
+
+};
+int main()
+{
+    emp e[5]; //array of structure variable(e[5])
+    int i;
+    for(i=0;i<5;i++)
+    {
+        cout<<"Enter the name of the employee no."<<i+1<<":";
+        cin>>e[i].name;
+
+    }
+    cout<<"\nName of all employees:\n";
+    for(i=0;i<5;i++)
+    {
+        cout<<e[i].name<<endl;
+    }
+   
+    return 0;
+}
