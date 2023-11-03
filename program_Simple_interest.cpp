a19f7750fbf503b0afa892bb7a358a2803d253a7
Smita <114800220+12217951@users.noreply.github.com>
2023-11-03 09:20:53 +0530

    Add files via upload

---
+++ program_Simple_interest.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float *p=NULL;
+    float *r =NULL;
+    float *t =NULL;
+    float *si=NULL;
+    p=new float;
+    r=new float;
+    t=new float;
+    si=new float;
+    if(p==NULL || r==NULL || t==NULL || si==NULL)
+    {
+        cout<<"\n Memory allocation failed ";
+        exit(1);
+    }
+    cout<<"\n Enter principle,rate,time";
+    cin>>*p>>*r>>*t;
+    *si=(0.01)*(*p)*(*r)*(*t);
+    cout<<"\n Simple Interest is: "<<*si;
+    delete p;
+    delete r;
+    delete t;
+    delete si;
+    return 0;
+}
