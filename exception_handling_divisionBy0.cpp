d8ff67cae129019ebf7823481b8e459c56b0cfa5
Smita <114800220+12217951@users.noreply.github.com>
2023-11-03 09:18:49 +0530

    Add files via upload

---
+++ exception_handling_divisionBy0.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+//     cout<<"Enter the values of a and b "<<endl;
+//     cin>>a>>b;
+//     int x=a-b;
+//     try
+//     {
+//         if(x!=0)
+//         {
+//             cout<<"result(a/b)"<<endl;
+//         }
+//         else{
+//             throw(x);
+//         }
+//         catch(int i)
+//         {
+//             cout<<"Excpetion caught:x= "<<x;
+//         }
+//     }
+//     catch(const std::exception& e)
+//     {
+//         cerr << e.what() <<"\n";
+//     }
+    
+// }
