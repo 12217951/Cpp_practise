25d5c16afd710936d1d0f8d01e38c4b356524d92
Smita <114800220+12217951@users.noreply.github.com>
2023-11-09 08:54:28 +0530

    Add files via upload

---
+++ str6.cpp
@@ -0,0 +1,60 @@
+// #include<iostream>
+// using namespace std;
+// class rectangle
+//     {
+//         public:
+//         int l,b;
+//         rectangle()
+//         {
+//             l=5,b=6;
+//         }
+//        rectangle(int length)
+//        {
+//         l=length;
+//         b=7;
+//        }
+//         void area()
+//         {
+//             cout<<l*b<<endl;
+//         }
+
+//     };
+//     int main()
+//     {
+//         rectangle r;
+//         int l;
+//         cin>>l;
+//         rectangle r2(l);
+//         r.area();
+//         r2.area();
+    
+//     return 0;
+//     }
+
+
+
+
+
+
+
+// class rect{
+//     int a,b;
+//     public:
+//     rect()
+//     {
+//         a=5,b=3;
+//     }
+//     rect(int x,int y){
+//         a=x,b=y;
+//     }
+//     void area();
+//     {
+//         cout<<a*b;
+//     }
+// };
+// int main()
+// {
+//     object.area;
+//     rect.obj2(5,4);
+
+//     }
