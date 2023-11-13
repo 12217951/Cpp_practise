ee69247caeb7590c0b5b11e5a91255c0bd17d895
Smita <114800220+12217951@users.noreply.github.com>
2023-11-13 10:43:13 +0530

    Add files via upload

---
+++ appendMod1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    char  s[30];
+    ofstream fileout;
+    ifstream filein;
+    fileout.open("data",ios::app);
+    fileout<<"hello world";
+    fileout.close();
+    filein.open("data",ios::in);              //in-->read
+    filein.getline(s,30);
+    cout<<s;
+
+}
