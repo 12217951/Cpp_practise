422850f28933539c78f44b9ba92a323bda98dd27
Smita <114800220+12217951@users.noreply.github.com>
2023-11-17 08:46:27 +0530

    Add files via upload

---
+++ string1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    string s1("12345");
+    string s2("abcde");
+    s1.append(s2);
+    cout<<s1<<endl;
+
+    s1.append(s2,1,2);
+    cout<<s1;
+
+}
