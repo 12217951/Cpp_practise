74c7506f4f55b39e0420e700ed9bc66245cf605c
Smita <114800220+12217951@users.noreply.github.com>
2023-11-22 12:17:41 +0530

    Add files via upload

---
+++ destructo1r.cpp
@@ -0,0 +1,25 @@
+//  when we call constr obj will be created allocating memory to deallocate memory we use destructor
+// no arguments and return type.
+// only one dest. is called.
+#include<iostream>
+// #include<conio.h>
+// using namespace std;
+// class counter()
+// {
+//     int id;
+//     public:
+//     counter(int i)
+//     {
+//         id=i;
+//         cout<<"Data member destroyed:"<<id;
+//     }
+//     ~counter()
+//     {
+//         cout<<"Dest with id +"<<id;
+//     }
+// };
+// counter c1(1);
+// counter c1(2);
+// counter c1(3);
+// cout<<"\n end of main";
+
